68d219aee913807ac4f1ec5f3177fff6c82325f0
Steve King <git@skzone.org>
2023-05-27 08:49:48 +0100

    save work in progress

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from app.routers.v1 import router as v1_router
+from app.services.scheduler import scheduler
+from app.services.processor import sync_roles
 
 app = FastAPI()
 app.include_router(v1_router, prefix="/api/v1")
@@ -8,5 +10,12 @@ app.include_router(v1_router, prefix="/api/v1")
 @app.get("/", include_in_schema=False)
 async def index():
     return RedirectResponse(url="/docs")
+
+
+@app.on_event("startup")
+async def load_schedule_or_create_blank():
+    scheduler.add_job(sync_roles, "date")
+    scheduler.add_job(sync_roles, "interval", minutes=1)
+    scheduler.start()
 
 
--- app/services/processor.py
+++ app/services/processor.py
@@ -1,24 +1,24 @@
-import schedule
 import time
 import requests
 
-def process_data(data):
+
+
+async def process_data(data):
     # Your code to process the data goes here
     print(data)
 
-def call_api():
+async def call_api():
     # Your code to call the API goes here
     response = requests.get("https://example.com/api/data")
     if response.status_code == 200:
         data = response.json()
-        process_data(data)
+        await process_data(data)
     else:
         print("Error calling API")
 
-# Schedule the task to run every 15 minutes
-schedule.every(15).minutes.do(call_api)
+async def sync_roles():
+    print("Syncing roles")
+    time.sleep(5)
+    print("Roles synced")
 
-# Run the scheduled task indefinitely
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+
