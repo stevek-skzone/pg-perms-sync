39ca8d42f9e3a588b09c35e402a2ea47db07d70e
Steve King <git@skzone.org>
2023-05-28 09:45:18 +0100

    work in progress

--- app/routers/endpoints/groups.py
+++ app/routers/endpoints/groups.py
@@ -3,38 +3,66 @@ from fastapi import APIRouter
 router = APIRouter()
 
 # Define some example data
+from fastapi import APIRouter
+
+router = APIRouter()
+
+# Define some example data
 test_data = {
     "server1": {
-        "group1": {
-            "metadata": {
-                "registered": "2022-01-01",
-                "processed": True
-            },
-            "members": [
-                {"name": "Alice", "roles": "admin,editor"},
-                {"name": "Bob", "roles": "editor"},
-                {"name": "Charlie", "roles": "viewer"}
-            ]
+        "metadata": {
+            "connection": "localhost:8000",
+            "last_processed": "2022-01-15T12:00:00Z"
         },
-        "group2": {
-            "metadata": {
-                "registered": "2022-01-02",
-                "processed": False
+        "groups": {
+            "group1": {
+                "metadata": {
+                    "registered": "2022-01-01",
+                    "processed": True,
+                    "roles": ["admin", "editor", "viewer"]
+                },
+                "members": [
+                    {"name": "Alice", "date_added": "2022-01-01T12:00:00Z"},
+                    {"name": "Bob", "date_added": "2022-01-01T12:00:00Z"},
+                    {"name": "Charlie", "date_added": "2022-01-01T12:00:00Z"}
+                ]
+            },
+            "group2": {
+                "metadata": {
+                    "registered": "2022-01-02",
+                    "processed": False,
+                    "roles": []
+                }
             }
         }
     },
     "server2": {
-        "group1": {
-            "metadata": {
-                "registered": "2022-01-03",
-                "processed": True
-            },
-            "members": [
-                {"name": "Frank", "roles": "viewer"}
-            ]
+        "metadata": {
+            "connection": "localhost:8001",
+            "last_processed": "2022-01-14T12:00:00Z"
+        },
+        "groups": {
+            "group1": {
+                "metadata": {
+                    "registered": "2022-01-03",
+                    "processed": True,
+                    "roles": ["viewer"]
+                },
+                "members": [
+                    {"name": "Frank", "date_added": "2022-01-03T12:00:00Z"}
+                ]
+            }
         }
     }
 }
+
+@router.get("/servers", description="Get servers")
+async def get_server_list():
+    servers = []
+    for server_name, values in test_data.items():
+        servers.append({"server_name": server_name, "connection": values["metadata"]["connection"]})
+
+    return servers
 
 
 @router.get("/server/{server_name}", description="Get server groups")
@@ -43,25 +71,25 @@ async def get_server(server_name: str):
         return {"error": "Server not found"}
 
     groups = []
-    for group in test_data[server_name]:
+    for group in test_data[server_name]["groups"]:
         groups.append(group)
 
     return {"server_name": server_name, "groups": groups}
 
 @router.get("/group/{server_name}/{group_name}", description="Get server group metadata")
 async def get_group(server_name: str, group_name: str):
-    if server_name not in test_data or group_name not in test_data[server_name]:
+    if server_name not in test_data or group_name not in test_data[server_name]["groups"]:
         return {"error": "Server or group not found"}
 
-    group = test_data[server_name][group_name]
-    metadata = {"registered": group["metadata"]["registered"], "processed": group["metadata"]["processed"]}
+    group = test_data[server_name]["groups"][group_name]
+    metadata = {"registered": group["metadata"]["registered"], 
+                "processed": group["metadata"]["processed"],
+                "roles": group["metadata"]["roles"]}
 
     if not group["metadata"]["processed"]:
         return {"server_name": server_name, "group_name": group_name, "metadata": metadata}
 
     members = group["members"]
-    for member in members:
-        member["roles"] = member["roles"].split(",")
 
     return {"server_name": server_name, "group_name": group_name, "metadata": metadata, "members": members}
 
@@ -71,7 +99,7 @@ async def get_all_members(server_name: str):
         return {"error": "Server not found"}
 
     members = []
-    for group_name, group in test_data[server_name].items():
+    for group_name, group in test_data[server_name]["groups"].items():
         if group["metadata"]["processed"]:
             for member in group["members"]:
                 member["roles"] = member["roles"].split(",")
--- app/services/processor.py
+++ app/services/processor.py
@@ -22,3 +22,38 @@ async def sync_roles():
     print("Roles synced")
 
 
+import requests
+
+async def get_local_groups():
+    # Your code to get local groups goes here
+    response = requests.get("http://localhost:8000/api/v1/groups")
+    if response.status_code == 200:
+        groups = response.json()
+        return groups
+    else:
+        print("Error getting local groups")
+
+async def get_external_group_members(group):
+    # Your code to get external group members goes here
+    response = requests.get(f"https://example.com/api/groups/{group}/members")
+    if response.status_code == 200:
+        members = response.json()
+        return members
+    else:
+        print(f"Error getting external group members for group {group}")
+
+async def sync_groups():
+    # Get local groups
+    local_groups = await get_local_groups()
+
+    # Loop through local groups and get external group members
+    for group in local_groups:
+        external_members = await get_external_group_members(group["name"])
+
+        # Compare external members with local members and sync
+        local_members = group["members"]
+        members_to_add = set(external_members) - set(local_members)
+        members_to_remove = set(local_members) - set(external_members)
+
+        # Your code to add or remove members goes here
+        print(f"Group {group['name']}: {len(members_to_add)} members to add, {len(members_to_remove)} members to remove")
